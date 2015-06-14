fd3cc8696a5f63aaae549c0c02f76f06c5d07795
GryfonN <petranikpeter@gmail.com>
2015-06-14 18:59:27 +0200

    sync inject and reload

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
+var gulpsync = require('gulp-sync')(gulp);
 var browserSync = require('browser-sync');
 
 function isOnlyChange(event) {
@@ -8,28 +9,44 @@ function isOnlyChange(event) {
 }
 
 module.exports = function (options) {
+
+  //only reload
+  gulp.task('browser-realod', browserSync.reload);
+
+  //Sync inject > Reload
+  gulp.task('injectAndReload', gulpsync.sync(['inject', 'browser-realod']));
+
+  //watcher
   gulp.task('watch', ['inject'], function () {
 
-    gulp.watch([options.src + '/*.html', 'bower.json'], options.gulpWatch, ['inject']);
+    //HTML index.html only
+    gulp.watch([options.src + '/*.html', 'bower.json'], options.gulpWatch, function (event) {
+      gulp.start('injectAndReload');
+    });
 
+    //CSS
     gulp.watch(options.src + '/app/**/*.css', options.gulpWatch, function (event) {
       if (isOnlyChange(event)) {
         browserSync.reload(event.path);
       } else {
-        gulp.start('inject');
+        gulp.start('injectAndReload');
       }
     });
 
+    //JS
     gulp.watch(options.src + '/app/**/*.js', options.gulpWatch, function (event) {
       if (isOnlyChange(event)) {
         gulp.start('scripts');
+        browserSync.reload(event.path);
       } else {
-        gulp.start('inject');
+        gulp.start('injectAndReload');
       }
     });
 
+    //HTML ngTemplates
     gulp.watch(options.src + '/app/**/*.html', options.gulpWatch, function (event) {
       browserSync.reload(event.path);
     });
   });
-};
+
+};//module.exports
